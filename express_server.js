10a0e4103a84d787d89718c0e756fcb832cbdc6b
firewallfail <firewall.fail@hotmail.com>
2022-04-19 12:47:37 -0400

    required cookie parser

--- express_server.js
+++ express_server.js
@@ -3,6 +3,7 @@ const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
+const cookieParser = require('cookie-parser');
 
 
 //convert a random number to hex and then take a 6 digit slice of it
