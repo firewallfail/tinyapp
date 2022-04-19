9940d73661d11887421bf6e936dcf4d7aadc2d53
firewallfail <firewall.fail@hotmail.com>
2022-04-18 21:16:28 -0400

    added comments to make it easier to read

--- express_server.js
+++ express_server.js
@@ -5,6 +5,11 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
+//convert a random number to hex and then take a 6 digit slice of it
+const generateRandomString = () => {
+  return Math.random().toString(36).slice(2, 8);
+};
+
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -15,55 +20,53 @@ const urlDatabase = {
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
-
 app.listen(PORT, () => {
   console.log(`TinyURL listening on port ${PORT}!`);
 });
-
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+//way to check all the values of the database as a json
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
+//page to add new urls
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
-
-app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
-});
-
+//response after adding a new page
 app.post("/urls", (req, res) => {
   let shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL;
   res.redirect(`/urls/${shortURL}`);
 });
 
-app.post("/urls/:id", (req, res) => {
-  urlDatabase[req.params.id] = req.body.longURL;
-  res.redirect(`/urls/${req.params.id}`);
+//page showing all current urls
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
-
-app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
-  res.render("urls_show", templateVars);
-});
-
+//url deletion from /urls
 app.post("/urls/:shortURL/delete", (req, res) => {
   delete urlDatabase[req.params.shortURL];
   res.redirect("/urls");
 });
 
+//edit page for a single url
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
+});
+//update link that the shortened url leads to
+app.post("/urls/:id", (req, res) => {
+  urlDatabase[req.params.id] = req.body.longURL;
+  res.redirect(`/urls/${req.params.id}`);
+});
+
+//redirect from the shortened url to the associated website
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
 
-//convert a random number to hex and then take a 6 digit slice of it
-const generateRandomString = () => {
-  return Math.random().toString(36).slice(2, 8);
-};
